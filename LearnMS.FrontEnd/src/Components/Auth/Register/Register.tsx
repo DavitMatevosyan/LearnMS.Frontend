ed6e514c761bee5956384d5dd39959ef44a429a7
Davit Matevosyan <Davit.Matevosyan@medfarsolutions.com>
2024-01-04 22:27:55 +0400

    added initial register form states

--- LearnMS.FrontEnd/src/Components/Auth/Register/Register.tsx
+++ LearnMS.FrontEnd/src/Components/Auth/Register/Register.tsx
@@ -1,8 +1,25 @@
-import { Form, Input } from "antd";
+import { Button, DatePicker, Form, Input } from "antd";
+
+import styles from "./Register.module.scss";
+import { useState } from "react";
+
+interface RegisterForm {
+    firstName: string;
+    lastName: string;
+    email: string;
+    dateOfBirth: Date;
+    courseReferralLink: string;
+}
 
 export default function RegisterForm(): JSX.Element {
+    const [firstName, setFirstName] = useState<string>("");
+    const [lastName, setLastName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [dateOfBirth, setDateOfBirth] = useState<Date>();
+    const [courseReferralLink, setCourseReferralLink] = useState<string>("");
+
     return (
-        <Form name="register-form">
+        <Form className={styles.form} name="register-form">
             <Form.Item
                 name="firstName"
                 label="First Name"
@@ -13,7 +30,7 @@ export default function RegisterForm(): JSX.Element {
                         }
                     ]
                 }>
-                <Input placeholder="First name"/>
+                <Input placeholder="First name" onChange={e => setFirstName(e.target.value)}/>
             </Form.Item>
             <Form.Item
                 name="lastName"
@@ -25,7 +42,44 @@ export default function RegisterForm(): JSX.Element {
                         }
                     ]
                 }>
-                <Input placeholder="Last name"/>
+                <Input placeholder="Last name" onChange={e => setLastName(e.target.value)}/>
+            </Form.Item>
+            <Form.Item
+                name="email"
+                label="Email"
+                rules={[{
+                            required: true,
+                            message: "Not valid Email"
+                        }
+                    ]
+                }>
+                <Input placeholder="Email" onChange={e => setEmail(e.target.value)}/>
+            </Form.Item>
+            <Form.Item
+                name="dateOfBirth"
+                label="Date of Birth"
+                rules={[{
+                            required: true,
+                            message: "Not valid date of birth"
+                        }
+                    ]
+                }>
+                <DatePicker placeholder="Date of Birth" onChange={(_, dob) => setDateOfBirth(new Date(dob))}/>
+            </Form.Item>
+            <Form.Item
+                name="referralLink"
+                label="Course Referral Link"
+                rules={[{
+                            required: true,
+                            len: 6,
+                            message: "Not valid Course Referral Link"
+                        }
+                    ]
+                }>
+                <Input placeholder="link" onChange={e => setCourseReferralLink(e.target.value)}/>
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary">Register</Button>
             </Form.Item>
         </Form>
     );
